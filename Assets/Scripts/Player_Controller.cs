e8783be2f89c32887f477ad87b24f8a0f246f7bc
dutudev <dutudev@gmail.com>
2023-11-17 18:29:36 +0200

    Cleaned up player controller

--- Assets/Scripts/Player_Controller.cs
+++ Assets/Scripts/Player_Controller.cs
@@ -7,10 +7,13 @@ public class Player : MonoBehaviour
     public Rigidbody rb;
     public Collider2D col;
     public GameObject rotObject;
+
     private bool isGrounded;
+
     private float moveSpeed = 50f;
     private float rotationSpeed = 500f;
     private float jumpForce = 500f;
+
     private Vector3 movementInput;
 
     void Update()
@@ -18,7 +21,7 @@ public class Player : MonoBehaviour
         // Read player input and set rotation
         movementInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
         rotObject.transform.position = transform.position + movementInput;
-
+        // Read player jump input
         if (Input.GetButtonDown("Jump") && isGrounded)
         {
             Jump();
@@ -27,12 +30,14 @@ public class Player : MonoBehaviour
 
     void FixedUpdate()
     {
+        // Move and rotate the player
         MovePlayer();
         RotatePlayer();
     }
 
     void OnCollisionEnter(Collision collision)
     {
+        // Ground check
         if (collision.gameObject.CompareTag("Ground"))
         {
             isGrounded = true;
@@ -41,6 +46,7 @@ public class Player : MonoBehaviour
 
     private void OnTriggerEnter(Collider col)
     {
+        // Destroy enemy if in contact with it
         if (col.gameObject.CompareTag("Enemy") && !isGrounded)
         {
             Destroy(col.gameObject);
@@ -49,21 +55,24 @@ public class Player : MonoBehaviour
 
     void MovePlayer()
     {
+        // Move player
         Vector3 newPosition = transform.position + movementInput * moveSpeed * Time.fixedDeltaTime;
         rb.MovePosition(newPosition);
     }
 
     void RotatePlayer()
     {
+        // Rotate the player if it isn't moving
         if (movementInput != Vector3.zero)
         {
             Quaternion toRotation = Quaternion.LookRotation(movementInput, Vector3.up);
             transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotationSpeed * Time.deltaTime);
         }
     }
-
+    
     void Jump()
     {
+        // Set the ground check to false
         isGrounded = false;
         rb.AddForce(new Vector3(0, jumpForce, 0));
     }
